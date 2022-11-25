2d77af1a59bf2f63c0fcce78432f6c5b8a418afb
wangzilong1997 <1321787581@qq.com>
2022-11-25 09:49:54 +0800

    :data: 5例子2

---
+++ 5例子2.py
@@ -0,0 +1,33 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+
+# create data
+x_data = np.random.rand(100).astype(np.float32)
+print('x_data',x_data)
+y_data = x_data*0.1 + 0.3
+print('y_data',y_data)
+### create tensorflow structure start ###
+
+
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zeros([1]))
+
+y = Weights*x_data + biases
+
+loss = tf.reduce_mean(tf.square(y-y_data))
+
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+# init = tf.initialize_all_variables() # tf 马上就要废弃这种写法
+init = tf.global_variables_initializer()  # 替换成这样就好
+### create tensorflow structure end ###
+
+sess = tf.Session()
+sess.run(init)          # Very important
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(Weights), sess.run(biases))
